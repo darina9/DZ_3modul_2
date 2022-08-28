7e8b73d3f00cdb4fca26b7d450830e8c1dccd5f9
darina9 <krastinadv@gmail.com>
2022-08-28 16:31:11 +0300

    Zadacha_15

---
+++ Zadacha_15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num==6 ||num==7 )
+{
+    Console.WriteLine("Это выходной!!!");
+}
+else if(num>7)
+{
+    Console.WriteLine("Это не день недели!!!");
+
+}
+else if(num>=1 && num <=5 )
+{
+    Console.WriteLine("Это будний день :(");}
