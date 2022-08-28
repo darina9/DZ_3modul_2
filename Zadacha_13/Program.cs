c2a03af447f397ba817fdf18af1a372d4689034b
darina9 <krastinadv@gmail.com>
2022-08-28 15:38:16 +0300

    .\Zadacha_13\

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите  число");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num<100)
+{
+    Console.WriteLine("В этом числе нет третьей цифры");
+}
+else if(num>100 && num<1000)
+{   
+    int num1= num%10;
+    Console.WriteLine("Третья цифра :" + num1);}
+
+else if(num>1000 && num<10000)
+
+{  
+    int num2= (num%100)/10;
+    Console.WriteLine("Третья цифра :" + num2);}
+
+else if(num>10000 && num<100000)
+
+{  
+    int num3= (num%1000)/100;
+    Console.WriteLine("Третья цифра :" + num3);}
